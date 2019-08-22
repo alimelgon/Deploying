106394777e24a025f57cec8f5c6c0bdd1c32721c
Alicia Melgar <alimelgon27@gmail.com>
2019-08-22 19:43:58 +0200

    yo misma

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return 'lo que queramos'
+    #return 'Loquequeramos'
+    return render_template('index.html')
